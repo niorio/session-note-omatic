4940ec98cacc3e6cb120608370566998cec05f21
Nick Iorio <niorio1@gmail.com>
2022-02-06 19:24:23 -0500

    tiny fix

--- pages/index.js
+++ pages/index.js
@@ -27,7 +27,7 @@ export default function Home() {
       <Head>
         <title>Session-note-omatic</title>
         <link rel="icon" href="/favicon.ico" />
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossOrigin="anonymous" />
       </Head>
 
       <main className='container'>
